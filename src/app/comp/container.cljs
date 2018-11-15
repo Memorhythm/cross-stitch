d81e60743d417cc996aa18cdf71d4a25495ebab5
jiyinyiyong <jiyinyiyong@gmail.com>
2018-11-16 02:32:55 +0800

    initial renderings with SVG

--- src/app/comp/container.cljs
+++ src/app/comp/container.cljs
@@ -7,11 +7,56 @@
             ["react-dom" :as ReactDOM]
             [reacher.core
              :refer
-             [create-comp div input span button a adorn dispatch! get-state get-value]]
+             [create-comp div input span button tag* a adorn dispatch! get-state get-value]]
             [respo-ui.core :as ui]
-            [reacher.comp :refer [=< comp-inspect]]))
+            [reacher.comp :refer [=< comp-inspect]]
+            [app.util :refer [expand-grid]]))
+
+(def comp-sudoku
+  (create-comp
+   {:key-fn (fn [x y] (str x ":" y)), :name :app-sudoku}
+   (fn [[x y] _ _]
+     (let [sx (* x 44), sy (* y 44)]
+       (tag*
+        :g
+        {}
+        (tag* :rect {:x (- sx 2), :y (- sy 2), :width 40, :height 40, :fill (hsl 0 80 80)})
+        (expand-grid
+         3
+         3
+         (fn [a b]
+           (tag*
+            :line
+            {:key (str a ":" b),
+             :x1 (+ sx (* a 12)),
+             :y1 (+ sy (* b 12)),
+             :x2 (+ sx (* 12 (+ a 1))),
+             :y2 (+ sy (* 12 (+ b 1))),
+             :stroke "white",
+             :stroke-width 2})))
+        (expand-grid
+         3
+         3
+         (fn [a b]
+           (tag*
+            :line
+            {:key (str a ":" b),
+             :x1 (+ sx (* (+ a 1) 12)),
+             :y1 (+ sy (* b 12)),
+             :x2 (+ sx (* 12 a)),
+             :y2 (+ sy (* 12 (+ b 1))),
+             :stroke "white",
+             :stroke-width 2}))))))))
+
+(def comp-field
+  (create-comp
+   {:name :app-field}
+   (fn [[store] _ _]
+     (tag* :svg {:width 400, :height 400} (expand-grid 4 4 (fn [x y] (comp-sudoku x y)))))))
 
 (def comp-container
   (create-comp
    {:state nil, :name :app-container}
-   (fn [[store] state mutate!] (div {:style (adorn ui/global {})} "Containers2"))))
+   (fn [[store] state mutate!]
+     (div {:style (adorn ui/global {})} "Containers2")
+     (comp-field))))
--- src/app/util.cljs
+++ src/app/util.cljs
@@ -1,4 +1,7 @@
 
 (ns app.util )
 
+(defn expand-grid [x y f]
+  (map (fn [xs] (apply f xs)) (for [x (range x), y (range y)] [x y])))
+
 (defn get-env! [property] (aget (.-env js/process) property))
